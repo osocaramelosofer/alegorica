9cdb71f849c597777815ce4457aaf355dc79cdd2
Austin <babelontrip@gmail.com>
2024-12-29 14:53:51 -0600

    wip hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,41 @@
+import { SpinningText } from "@/components/ui/spinning-text";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_500px_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
       <nav>
-        <ul className="flex gap-x-2">
+        <ul className="flex gap-x-5 items-center">
+          <li>
+            <span className="font-chomsky text-2xl font-normal">Alegorica</span>
+          </li>
           <li>tienda</li>
           <li>nuevo</li>
           <li>colecciones</li>
         </ul>
       </nav>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="font-chomsky text-2xl font-normal">Alegorica</h1>
-        <h1 className="font-times text-2xl font-normal">Times New Roman</h1>
+      <main className="relative flex flex-col gap-8 row-start-2 items-center sm:items-start w-full h-full">
+        {/* <div className="absolute inset-0 bg-black ">
+        </div> */}
+      
+          <div className="bg-opacity-50 flex w-full justify-center items-center absolute  h-full">
+            <span className="font-chomsky text-[30vw] font-normal   tracking-tighter" aria-hidden>alegórica</span>
+          </div>
+          <h2 className="font-playFair text-black" >Sé parte del club</h2>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <section>
+        <h2 className="font-times text-6xl">Prendas</h2>
+      </section>
+      <footer className="flex gap-6 flex-wrap items-center justify-center">
+      <div className="mr-10">
+              <SpinningText
+                  radius={4.5}
+                  fontSize={.8}
+                  className='font-medium leading-none tracking-widest'
+                >
+              {`alegorica • alegorica • `}
+              </SpinningText>  
+            </div>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -28,7 +49,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Discover
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -43,7 +64,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Collection
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -58,7 +79,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          About us →
         </a>
       </footer>
     </div>
