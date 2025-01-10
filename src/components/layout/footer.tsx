b0c0bdfb70d92b7f8ba533a894efd4ac0f6cb69c
Austin <babelontrip@gmail.com>
2025-01-10 14:25:56 -0600

    wip add logo footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,43 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
 import { cn } from '@/lib/utils/utils'
-import { SpinningText } from '../ui/spinning-text'
-import Image from 'next/image'
 
 interface FooterProps {
   className: string
 }
 export default function Footer({ className }: FooterProps) {
   return (
-    <footer
-      className={cn(
-        'flex row-start-5 h-full gap-6 flex-wrap items-center justify-center',
-        className
-      )}
-    >
-      <div className="mr-10">
-        <SpinningText
-          radius={4.5}
-          fontSize={0.8}
-          className="font-medium leading-none tracking-widest"
-        >
-          {`alegorica • alegorica • `}
-        </SpinningText>
-      </div>
+    <footer className={cn(' row-start-5 h-full gap-6 w-full mt-32', className)}>
+      <div className="flex justify-between">
+        <div className="h-32">
+          <img
+            className="w-full h-full object-cover"
+            src="/images/enamorados-encuerados-logo.webp"
+            alt="enamorados encuerados logo"
+          />
+        </div>
 
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://www.instagram.com/alegoricaclub/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/globe.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
-        Siguenos →
-      </a>
+        {/* <div className="">
+          <ul className="flex flex-col">
+            <li>
+              <a
+                className="font-playFair text-xl font-semibold flex items-center gap-2 hover:underline hover:underline-offset-4"
+                href="https://www.instagram.com/alegoricaclub/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  aria-hidden
+                  src="/globe.svg"
+                  alt="Globe icon"
+                  width={16}
+                  height={16}
+                />
+                Siguenos
+              </a>
+            </li>
+            <li>
+              <a
+                className="font-playFair text-xl font-semibold flex items-center gap-2 hover:underline hover:underline-offset-4"
+                href="https://www.instagram.com/alegoricaclub/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  aria-hidden
+                  src="/globe.svg"
+                  alt="Globe icon"
+                  width={16}
+                  height={16}
+                />
+                Siguenos
+              </a>
+            </li>
+          </ul>
+        </div> */}
+      </div>
     </footer>
   )
 }
