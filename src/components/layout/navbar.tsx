88fe7f45a60e5ded78b8d999c4478e0020ed69b8
Austin <babelontrip@gmail.com>
2025-01-10 13:46:09 -0600

    change navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -7,13 +7,12 @@ interface NavbarProps {
 export default function Navbar({ className }: NavbarProps) {
   return (
     <nav className={cn('', className)}>
-      <ul className="italic flex gap-x-2 md:gap-x-10 md:text-2xl text-sm items-center font-times uppercase leading-10">
-        {/* <li className="font-chomsky text-2xl md:text-5xl font-normal normal-case">
-          Alegórica
-        </li> */}
-        <li>tienda</li>
-        <li>nosotros</li>
-        <li>galería</li>
+      <ul className="flex gap-x-2 md:gap-x-10 md:text-2xl text-sm items-center font-playFair">
+        <li>alegórica</li>
+        <li>nuevos</li>
+        <li>acerca</li>
+        <li>contacto</li>
+        <li>sociales</li>
       </ul>
     </nav>
   )
